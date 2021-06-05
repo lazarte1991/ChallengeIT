c56ce5c88beaf3b2749d8e1e54afa042deda4493
Leandro <lazarteleandro5@gmail.com>
2021-06-05 00:02:29 -0300

    Realizado el despliegue de detalles de los eventos

--- app/src/main/java/com/intermedia/challenge/ui/events/EventsAdapter.kt
+++ app/src/main/java/com/intermedia/challenge/ui/events/EventsAdapter.kt
@@ -1,12 +1,16 @@
 package com.intermedia.challenge.ui.events
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.intermedia.challenge.R
 import com.intermedia.challenge.data.models.Event
 import com.intermedia.challenge.ui.base.BaseAdapter
 import com.intermedia.challenge.databinding.ViewEventItemBinding
+import kotlinx.android.synthetic.main.view_event_item.*
+import kotlinx.android.synthetic.main.view_event_item.view.*
+import android.graphics.drawable.Drawable as Drawable
 
 class EventsAdapter : BaseAdapter<Event, EventsAdapter.EventsViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventsViewHolder =
@@ -31,9 +35,20 @@ class EventsAdapter : BaseAdapter<Event, EventsAdapter.EventsViewHolder>() {
 
         fun bind(item: Event) = with(itemView) {
             binding.event = item
-            binding.root.setOnClickListener {
-                onClickListener?.invoke(item)
+
+            binding.button2.setOnClickListener {
+
+                    if(comicLayout.visibility == View.GONE){
+                        comicLayout.visibility = View.VISIBLE
+                        binding.button2.setImageResource(R.mipmap.ic_up_icon)
+                    }else{
+                        comicLayout.visibility = View.GONE
+                        binding.button2.setImageResource(R.mipmap.ic_down_icon)
+                    }
+
             }
         }
     }
 }
+
+
--- app/src/main/java/com/intermedia/challenge/ui/events/EventsFragment.kt
+++ app/src/main/java/com/intermedia/challenge/ui/events/EventsFragment.kt
@@ -2,12 +2,15 @@ package com.intermedia.challenge.ui.events
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.intermedia.challenge.databinding.FragmentEventsBinding
+import kotlinx.android.synthetic.main.view_event_item.*
+import kotlinx.android.synthetic.main.view_event_item.view.*
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 
 
@@ -33,11 +36,16 @@ class EventsFragment : Fragment() {
         setupEventsList()
     }
 
-    private fun Context?.toast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) = this?.let { Toast.makeText(it, text, duration).show() }
+    //private fun Context?.toast(text: CharSequence, duration: Int = Toast.LENGTH_LONG) = this?.let { Toast.makeText(it, text, duration).show() }
 
     private fun setupEventsList() {
+
+
         adapter.onClickListener = { event ->
-            context?.toast(event.title, Toast.LENGTH_LONG)
+
+
+
+           // context?.toast(event.title, Toast.LENGTH_LONG)
             // TODO complete
         }
         binding.listEvents.adapter = adapter
