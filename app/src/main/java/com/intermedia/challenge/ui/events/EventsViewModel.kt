689ef49b545bdccd3feb6f0badedfd2d10aac04e
Leandro <lazarteleandro5@gmail.com>
2021-06-07 11:07:07 -0300

    Hecha la logica del ordenamiento de fechas

--- app/src/main/java/com/intermedia/challenge/ui/events/EventsViewModel.kt
+++ app/src/main/java/com/intermedia/challenge/ui/events/EventsViewModel.kt
@@ -1,14 +1,16 @@
 package com.intermedia.challenge.ui.events
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.intermedia.challenge.data.models.Character
 import com.intermedia.challenge.data.models.Event
 import com.intermedia.challenge.data.models.NetResult
 import com.intermedia.challenge.data.repositories.EventsRepository
 import kotlinx.coroutines.launch
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EventsViewModel (private val eventsRepository: EventsRepository) : ViewModel() {
 
@@ -25,18 +27,27 @@ class EventsViewModel (private val eventsRepository: EventsRepository) : ViewMod
             when (val response = eventsRepository.getEvents(offset)) {
                 is NetResult.Success -> {
 
-                    list.addAll(response.data.eventsList.events.sortedByDescending { it.start })
+                    //Trae 15 eventos futuros (en la api hay solo uno a partir de la fecha de hoy)
+                    val date = SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(Date())
+                    var limit = 0
+                    val totalEvents = response.data.eventsList.events
+                    for (i in totalEvents){
+                        if (i.start != null && i.start >= date && limit <= 15){
+                            list.add(i)
+                            limit ++
+                        }
+                    }
+
+                    //Logica para traer todos los eventos sin importar la fecha
+                    //list.addAll(response.data.eventsList.events.sortedBy { it.start })
+
                     _events.postValue(list.sortedBy { it.start })
 
                 }
                 is NetResult.Error -> {
-                    // TODO complete
+                    Log.e(response.toString(),"ERROR")
                 }
             }
         }
-    }
-
-    fun loadMoreEvents() {
-        // TODO complete
     }
 }
